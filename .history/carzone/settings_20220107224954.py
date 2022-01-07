586408b101aa2be44601880cd09a9966bb936360
multisanjeev <sanjeev.katiyar2@gmail.com>
2022-01-07 22:52:33 +0530

    change in runtime file

--- .history/carzone/settings_20220107224954.py
+++ .history/carzone/settings_20220107224954.py
@@ -165,7 +165,7 @@ SITE_ID = 1
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_USER = 'jimmy.katiyar11@gmail.com'
 EMAIL_HOST_PASSWORD = 'xxxx'
 EMAIL_PORT = 587
 
